033f2b1a0b16432d0588ba6782b8d16f79f65c25
Getúlio Vargas <58775254+getvargas2042@users.noreply.github.com>
2020-07-31 22:27:49 -0300

    Update conversorCambio.java
    Correcoes da aula

--- Cambio/conversorCambio.java
+++ Cambio/conversorCambio.java
@@ -1,4 +1,4 @@
-public class conversorCambio{
+public class ConversorCambio{
     double valorReal;
     
     public conversorCambio(double real){
@@ -20,12 +20,13 @@ public class conversorCambio{
     public static void main(String[] args){
         conversorCambio cambio1 = new conversorCambio(1);
         System.out.println("R$1 para Dolar: "+cambio1.converterDolar());
-        
-        conversorCambio cambio2 = new conversorCambio(1);
         System.out.println("R$1 para Euro: "+cambio1.converterEuro());
-        
-        conversorCambio cambio3 = new conversorCambio(1);
         System.out.println("R$1 para Libra: "+cambio1.converterLibra());
+        
+        conversorCambio cambio2 = new conversorCambio(2);
+        System.out.println("R$2 para Dolar: "+cambio1.converterDolar());
+        System.out.println("R$2 para Euro: "+cambio1.converterEuro());
+        System.out.println("R$2 para Libra: "+cambio1.converterLibra());
     }
 }
 
